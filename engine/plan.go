4a72b5313465f5627934e44b1ac35176075af35c
Patrik Chytil <patrik.chytil@firma.seznam.cz>
2021-04-30 14:05:20 +0200

    work on solutions plan

--- engine/plan.go
+++ engine/plan.go
@@ -1,5 +1,11 @@
 package engine
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 //NodeType enum type.
 type NodeType int
 
@@ -13,16 +19,103 @@ const (
 type Node struct {
 	NodeType NodeType
 	Children []*Node
-	Data     string
+	ID       string
 	Parent   *Node
+}
+
+//String returns node representation.
+func (n *Node) String() string {
+	return fmt.Sprintf("id=%s, type=%d", n.ID, n.NodeType)
+}
+
+//CreateNode creates new node with specified ID.
+func CreateNode(ID string) *Node {
+	ID = strings.TrimSpace(ID)
+	nodeType := IDNode
+	if len(ID) == 1 {
+		_, err := strconv.Atoi(ID)
+		if err == nil {
+			nodeType = ValueNode
+		}
+	}
+	node := Node{NodeType: nodeType, ID: strings.ToLower(ID)}
+	return &node
+}
+
+//AddChild method adds new node to child collcetion.
+func (n *Node) AddChild(childNode *Node) {
+	childNode.Parent = n
+	n.Children = append(n.Children, childNode)
 }
 
 //Plan represents all found solutions of 1 game.
 type Plan struct {
-	Paths []Node
+	Paths []*Node
 }
 
-//FindSolutions method searches poseible solution paths for game.
+//AddNode insert new node into plan.
+func (p *Plan) AddNode(node *Node, rootNodeID string, parentPath string) error {
+	if len(rootNodeID) == 0 {
+		p.Paths = append(p.Paths, node)
+		return nil
+	}
+	var rootNode *Node
+	for _, rootNode = range p.Paths {
+		if rootNode.ID == rootNodeID {
+			break
+		}
+	}
+	if rootNode == nil {
+		return fmt.Errorf("RootNodeID: %s was not found in plan", rootNodeID)
+	}
+	if len(parentPath) == 0 {
+		rootNode.AddChild(node)
+		return nil
+	}
+	parentNodes := strings.Split(parentPath, "/")
+	wantedNode := p.findNodeByID(rootNode, parentNodes[0])
+	if wantedNode == nil {
+		return fmt.Errorf("ParentNodeID: %s was not found in plan", parentNodes[0])
+	}
+	wantedChildNode := p.findNodeByID(wantedNode, parentNodes[1])
+	if wantedChildNode == nil {
+		return fmt.Errorf("ParentNodeID: %s was not found in plan", parentNodes[1])
+	}
+	wantedChildNode.AddChild(node)
+	return nil
+}
+
+//FindNode method searches for node by ID in branch which is expressed by rootNodeId.
+func (p *Plan) FindNode(rootNodeID string, wantedNodeID string) (*Node, error) {
+	var root *Node
+	if len(rootNodeID) == 0 {
+		for _, root = range p.Paths {
+			if root.ID == strings.ToLower(wantedNodeID) {
+				break
+			}
+		}
+	}
+	if root == nil {
+		return nil, fmt.Errorf("RootNode Id: %s was not found in plan", rootNodeID)
+	}
+	return p.findNodeByID(root, wantedNodeID), nil
+}
+
+//findNodeByID method searches for node by ID in rootNode branch.
+func (p *Plan) findNodeByID(rootNode *Node, wantedNodeID string) *Node {
+	for _, child := range rootNode.Children {
+		if child.ID == strings.ToLower(wantedNodeID) {
+			return child
+		}
+		subChild := p.findNodeByID(child, wantedNodeID)
+		if subChild != nil {
+			return subChild
+		}
+	}
+	return nil
+}
+
+//FindSolutions method searches for possible solution paths for game.
 func (p *Plan) FindSolutions() (*bool, error) {
 	return nil, nil
 }
--- engine/plan_test.go
+++ engine/plan_test.go
@@ -1,1 +1,46 @@
 package engine
+
+import (
+	"testing"
+)
+
+func createPlan() *Plan {
+	plan := Plan{}
+	plan.AddNode(CreateNode("a"), "", "")
+	plan.AddNode(CreateNode("b"), "", "")
+	plan.AddNode(CreateNode("c"), "", "")
+	plan.AddNode(CreateNode("a1"), "a", "")
+	plan.AddNode(CreateNode("a2"), "a", "")
+	plan.AddNode(CreateNode("a11"), "a", "a1")
+	plan.AddNode(CreateNode("a12"), "a", "a1")
+	plan.AddNode(CreateNode("a111"), "a", "a11")
+	plan.AddNode(CreateNode("a112"), "a", "a11")
+	plan.AddNode(CreateNode("a113"), "a", "a11")
+	return &plan
+}
+
+func TestPlanShouldFindNode(t *testing.T) {
+	expectedID := "a113"
+	plan := createPlan()
+	node, err := plan.FindNode("a", expectedID)
+	if err != nil {
+		t.Errorf("FindNode should return node, but appears error: %v", err)
+	}
+	if node.ID != expectedID {
+		t.Errorf("FindNode returns node: %s, but expected nodeID: %s", node, expectedID)
+	}
+	expectedParentID := "a11"
+	if node.Parent.ID != expectedParentID {
+		t.Errorf("FindNode parent node id: %s, but expected nodeID: %s", node.Parent.ID, expectedParentID)
+	}
+	expectedParentParentID := "a1"
+	if node.Parent.Parent.ID != expectedParentParentID {
+		t.Errorf("FindNode parent parent node id: %s, but expected nodeID: %s", node.Parent.Parent.ID,
+			expectedParentParentID)
+	}
+	expectedParentParentParentID := "a"
+	if node.Parent.Parent.Parent.ID != expectedParentParentParentID {
+		t.Errorf("FindNode parent parent parent node id: %s, but expected nodeID: %s",
+			node.Parent.Parent.Parent.ID, expectedParentParentParentID)
+	}
+}
